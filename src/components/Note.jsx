d518bf5cf1f55e98e91e92d183714e65c58515cc
MinhVinh-37 <nguyenminhvinh37@gmail.com>
2024-08-18 17:30:09 +0700

    Init project

---
+++ src/components/Note.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Note() {
+  return (
+    <div className="note">
+      <h1>I'm Zico</h1>
+      <p>And who the fuck are you?</p>
+      <button>DELETE</button>
+    </div>
+  );
+}
